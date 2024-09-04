72d9551801a310dca09e6339f369c33c81faf0cc
Егор Чарушин <egor.charus2402@mail.ru>
2024-09-04 10:18:44 +0300

    ДКР №7

---
+++ DKR_7/DKR_7.pas
@@ -0,0 +1,59 @@
+﻿program DKR_7;
+uses GraphABC, System;
+
+var
+  scale: Real = 1.0; // параметризация
+  depth: Integer = 6;
+  offsetX: Real = 0.0;
+  offsetY: Real = 0.0;
+
+procedure Redraw; // для перерисовки окна
+begin
+  ClearWindow;
+  Draw(Round(100*scale+offsetX), Round(400*scale+offsetY), Round(700*scale+offsetX), Round(400*scale+offsetY), depth);
+end;
+
+procedure Button(Key: Integer); // нажатие кнопок
+begin
+  case Key of
+    VK_Down: begin 
+      offsetY := offsetY - 30;
+      Redraw;
+    end;
+    VK_Up: begin 
+      offsetY := offsetY + 30;
+      Redraw;
+    end;
+    VK_Right: begin 
+      offsetX := offsetX - 30;
+      Redraw;
+    end;
+    VK_Left: begin 
+      offsetX := offsetX + 30;
+      Redraw;
+    end;
+    VK_O: begin 
+      scale := scale * 1.5;
+      Redraw;
+    end;
+    VK_P: begin 
+      scale := scale / 1.5;
+      Redraw;
+    end;
+    VK_W: begin 
+      depth := depth + 1;
+      Redraw;
+    end;
+    VK_S: if depth > 0 then begin 
+      depth := depth - 1;
+      Redraw;
+    end;
+  end;
+end;
+
+begin
+  SetWindowSize(600, 600);
+  SetWindowCaption('Кривая Хартера-Хейтуэя');
+  OnKeyDown := Button;
+  Redraw;
+end.
---
+++ DKR_7/System.pas
@@ -0,0 +1,27 @@
+﻿unit System;
+
+interface // создание интрефейса
+
+uses GraphABC;
+
+procedure Draw(x1, y1, x2, y2, depth: integer);
+
+implementation // реализация модуля
+
+procedure Draw(x1, y1, x2, y2, depth: integer); 
+begin
+  if depth = 0 then // выделение базы рекурсии
+    Line(x1, y1, x2, y2)
+  else
+  begin
+    // координаты середины отрезка
+    var midX := (x1 + x2) div 2 + (y2 - y1) div 2;
+    var midY := (y1 + y2) div 2 - (x2 - x1) div 2;
+    
+    // Декомпозиция
+    Draw(x1, y1, midX, midY, depth - 1); // от (x1, y1) до середины (midX, midY),
+    Draw(x2, y2, midX, midY, depth - 1); // от (x2, y2) до середины (midX, midY)
+  end;
+end;
+
+end.
